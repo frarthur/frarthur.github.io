67b47b9d380f0402126bd94e6707dd4901b3add2
ArthurFr <arthur.fresse@etu.estia.fr>
2024-07-30 21:53:56 +0200

    Modif ajout langue image

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 function switchLanguage(language) {
     const elements = document.querySelectorAll('[data-' + language + ']');
     elements.forEach(element => {
-        element.textContent = element.getAttribute('data-' + language);
+        element.innerHTML = element.getAttribute('data-' + language);
     });
 }
+
+// Set default language to French
+switchLanguage('fr');
